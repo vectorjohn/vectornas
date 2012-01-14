d8d658d04141bb2fe790dda15ad66e3c21445def
John Reeves <john@jobshouts.com>
2012-01-14 01:10:17 -0800

    some web side stuff, and a demo of file tree

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,51 @@
+function Nas( cmd, done )
+{
+	$.getJSON( Nas.conf.url + cmd, done );
+}
+
+Nas.conf = {
+	url: '/v/'
+};
+
 $(function()
 {
+	var list = $('<ul/>')
+		.append( '<li><a href="#files/">Files</a></li>' )
+		.children( 'a' )
+			.end()
+		.appendTo( '#page-content' );
 
+	list.delegate( 'a', 'click', function( ev )
+	{
+		console.log( ev );
+
+		ev.preventDefault();
+		ev.stopPropagation();
+
+		var node = $(ev.target);
+		var path = node.attr( 'href' ).substr( 1 );
+
+		if ( node.children( 'ul' ).length )
+		{
+			node.children( 'ul' ).remove();
+			return;
+		}
+
+		Nas( path, function( res )
+		{
+			var ul = $('<ul/>');
+
+			res.files.sort();
+			_.each( res.files, function( f )
+			{
+				$('<li/>')
+					.append( $('<a/>')
+							.attr( 'href', '#' + path + f + '/' )
+							.text( f ) )
+					.appendTo( ul );
+			});
+
+			node.append( ul );
+		});
+	});
 });
